9d30d4e6f2d8f2a879b596f7fa30d412decbf9aa
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2020-02-25 16:58:40 -0300

    Add files via upload

--- client_pub_sub.py
+++ client_pub_sub.py
@@ -1,27 +1,35 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# Andre Augusto Giannotti Scota (https://sites.google.com/view/a2gs/)
-import sys
+from sys import exit, argv
 import zmq
 
 port = "5563"
+serv = "localhost"
+
+if len(argv) == 3:
+    serv = argv[1]
+    port = argv[2]
+elif len(argv) == 1:
+    print(f'Valore padrao server [{serv}] porta [{port}]\n')
+else:
+    print(f'Uso: {argv[0]} <SERVER_ADDRESS> <SERVER_PORT>\n')
+    exit(0);
 
 # Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 
-print("Collecting updates from server...")
-socket.connect (f"tcp://localhost:{port}")
+connectTo = f"tcp://{serv}:{port}"
+
+print(f"Coletando mensagens do server: [{connectTo}]")
+socket.connect(connectTo)
 
 while True:
     print("Waiting...")
-    topicfilter = b"10101"
+    topicfilter = b"10001"
     socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
 
     msg = socket.recv()
 
-    print(f"Recv: [{msg}]")
+    print(f"Recebido: [{msg[6:]}]\n")
 
 print("Pausa.")
 input()
